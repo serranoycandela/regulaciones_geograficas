d5716e83e9fa7ccad5ef3d7ba959b1e8e0d51c7f
serranoycandela <serranoycandela@gmail.com>
2021-11-23 21:14:34 -0600

    hectareas

--- yucAgebsAppRest/app/views.py
+++ yucAgebsAppRest/app/views.py
@@ -15,22 +15,32 @@ from sqlalchemy import create_engine, select, func
 from geoalchemy2 import functions  # NOQA
 
 import pandas as pd
+import geojson
+from utm_zone import epsg
 
-def area_interseccion(ageb, poligono):
+
+def area_interseccion(ageb, poligono,utm):
     # Geometría de la intersección del polígono de entrada con el ageb
     interseccion=session.scalar(ageb.geom.ST_Intersection(WKTElement(str(shape(poligono)), srid=4326)))
     # Convertimos la geometría de la intersección en formato WKTElement
     interseccion_wkte=WKTElement(str(to_shape(interseccion)),srid=4326)
     # Obtenemos el área de la geometría de la interseccion
-    area_interseccion=session.scalar(select([func.ST_Area(func.ST_Transform(interseccion_wkte,4486))]))
+    area_interseccion=session.scalar(select([func.ST_Area(func.ST_Transform(interseccion_wkte,utm))]))/10000
 
     return area_interseccion
 
-def area_poligono(ageb):
+def area_poligono(f,utm):
     # Obtenemos el área de ageb
-    area_ageb=session.scalar(ageb.geom.ST_Transform(4486).ST_Area())
+    
+    area_f = session.scalar(f.geom.ST_Transform(utm).ST_Area())/10000
 
-    return area_ageb
+    return area_f
+
+def get_UTM_from_WKT(wkt):
+    g1 = shapely.wkt.loads(wkt)
+    g2 = geojson.Feature(geometry=g1, properties={})
+    return epsg(g2)
+
 
 @app.route('/', methods=['GET'])
 @cross_origin(supports_credentials=True)
@@ -46,27 +56,20 @@ def interseccion(poligono):
         # Realizamos la consulta con geoalchemy2
         query= session.query(UgasLineamiento).filter(UgasLineamiento.geom.ST_Intersects(WKTElement(str(shape(poligono)), srid=4326)))
 
-        # Iteramos sobre los agebs que hace intersección el polígono recibido
-        for unidad in query:
-            # Imprimimos la cvegeo y la clave de la ageb
-            print(unidad.clave_uga, unidad.lineamiento_uga)
-            # Imprimimos el área del ageb
-            #print(session.scalar(unidad.geom.ST_Transform(4486).ST_Area()))
-            # Geometría de la intersección de polígono de entrada con el ageb
-            #interseccion=session.scalar(unidad.geom.ST_Intersection(WKTElement(str(shape(poligono)), srid=4326)))
-            #area_interseccion=session.scalar(select([func.ST_Area(func.ST_Transform(WKTElement(str(to_shape(session.scalar(unidad.geom.ST_Intersection(WKTElement(str(shape(poligono)), srid=4326))))),srid=4326),4486))]))
-            #print(str(area_interseccion/session.scalar(unidad.geom.ST_Transform(4486).ST_Area())))
-        # Convertimos la geometría de los agebs y los almacenamos en un diccionario
         geoms = {uga.id_uga:shapely.geometry.geo.mapping(to_shape(uga.geom)) for uga in query}
-
+        poligono_wkt = WKTElement(str(shape(poligono)), srid=4326)
+        utm = get_UTM_from_WKT(poligono_wkt.desc)
+        area_poligono_wkt = session.scalar(select([func.ST_Area(func.ST_Transform(poligono_wkt,utm))]))/10000
+        #print(type(poligono_wkt).__name__)
         # Iteramos sobre la consulta de las intersecciones del polígono con las agebs para generar un diccionario,
         # el cual se regresará en formato geojson en la consulta REST
         ugas = [{"type": "Feature",
             "properties":{"clave_uga":uga.clave_uga,
                           "lineamiento_uga":uga.lineamiento_uga,
-                          "area":round(area_poligono(uga),2),
-                          "area_interseccion": round(area_interseccion(uga, poligono),2),
-                          "porcentaje_interseccion": round((area_interseccion(uga, poligono)/area_poligono(uga))*100,4)
+                          "area_uga":round(area_poligono(uga, utm), 1),
+                          "area_poligono":round(area_poligono_wkt, 1),
+                          "area_interseccion": round(area_interseccion(uga, poligono, utm),1),
+                          "porcentaje_interseccion": round((area_interseccion(uga, poligono, utm)/area_poligono_wkt)*100, 1)
                           },
             "geometry":{"type":"MultiPolygon",
             "coordinates":geoms[uga.id_uga]["coordinates"]},
@@ -77,32 +80,3 @@ def interseccion(poligono):
 
 
 
-@app.route('/fomix/api/v0.1/municipios/coneval/<anio>/<indicador>', methods=['GET'])
-def municipios(anio,indicador):
-        # Realizamos una consulta de la cvegeo y la geometría de los municipios de Yucatán
-        muni_yucatan = pd.read_sql(session.query(TbMunicipiosyucatan.cvegeo,TbMunicipiosyucatan.geom).statement,session.bind)
-
-        # Convertimos la geometría de los municipios
-        muni_yucatan['geom'] = muni_yucatan['geom'].apply(lambda x: shapely.geometry.geo.mapping(to_shape(x)))
-
-        # Realizamos una consulta para obtener la tabla tb_yuc_coneval
-        coneval_yuc = pd.read_sql(session.query(t_tb_yuc_coneval).filter(t_tb_yuc_coneval.c.anio==anio,t_tb_yuc_coneval.c.indicador==indicador).statement,session.bind)
-        print(coneval_yuc)
-        # Hacemos un left join para agregar la geometría a coneval_yuc
-        coneval_yuc_geom = pd.merge(left=coneval_yuc, right=muni_yucatan, how='left', left_on='cvegeo', right_on='cvegeo')
-
-        # Nos quedamos con la geometría del polígono del municipio
-        coneval_yuc_geom['geom'] = coneval_yuc_geom['geom'].apply(lambda x: x['coordinates'])
-
-        # Iteramos sobre la reunión para generar un diccionario el cual se regresará en formato geojson en la consulta REST
-        coneval_yuc_dict = [{"type": "Feature",
-            "properties":{"cvegeo": row['cvegeo'],
-                          "anio": row['anio'],
-                          "indicador": row['indicador'],
-                          "valor_indicador": row['valor_indicador']
-                          },
-            "geometry":{"type":"MultiPolygon",
-            "coordinates":row['geom']},
-            } for index, row in coneval_yuc_geom.iterrows()]
-
-        return jsonify({"type": "FeatureCollection","features":coneval_yuc_dict})
